f672ce155f973ed60f6ac3d60f74bb535bd1ed27
Yash Poojara <yashpoojara268@gmail.com>
2021-06-29 10:57:55 +0530

    Completed Till models

--- app/models.py
+++ app/models.py
@@ -6,3 +6,14 @@ class Seller(models.Model):
     lname = models.CharField(max_length=255,default="Lastname")
     email = models.EmailField(unique=True)
     passwd = models.CharField(max_length=225,default="Password")
+
+class Category(models.Model):
+    Cname = models.CharField(max_length=255,default="Book Category")
+
+class Book(models.Model):
+    Book_name = models.CharField(max_length=255,default="BookName")
+    Author_name = models.CharField(max_length=255,default="Author name")
+    Book_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    Book_img = models.ImageField(upload_to="bookimages/",default="abc.jpg")
+    Book = models.FileField(upload_to="books",default="book1.pdf")
+    BookPrice = models.FloatField(default=0.0)
--- project/settings.py
+++ project/settings.py
@@ -74,10 +74,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'books',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
